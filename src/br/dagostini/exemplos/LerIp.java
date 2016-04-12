8d58db88c4124788b22baac83ace132448bf6e72
asrocha92 <asrocha92@gmail.com>
2016-04-12 20:39:54 -0300

    Modificado a classe LerIP, criado um método para retornar IP que a sua máquina está usando na rede.

--- src/br/dagostini/exemplos/LerIp.java
+++ src/br/dagostini/exemplos/LerIp.java
@@ -3,22 +3,33 @@ package br.dagostini.exemplos;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import javax.swing.JOptionPane;
+
 public class LerIp {
 
 	public LerIp() {
 
-		InetAddress IP;
 
+	}
+	
+	
+	public String retornarIP(){
+		InetAddress IP;
+		
 		try {
 			IP = InetAddress.getLocalHost();
 			String IPString = IP.getHostAddress();
-			System.out.println("Meu IP é: " + IP.getHostAddress());
+			
+			if (IPString.matches("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}")) {
+				return IPString;
+			}
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
+		return null;
 	}
 
 	public static void main(String[] args) {
-		new LerIp();
+		System.out.println(new LerIp().retornarIP());
 	}
 }
